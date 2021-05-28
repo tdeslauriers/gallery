092308c9aae059fa9430919f14532e84231b392d
tdeslauriers <thomas.deslauriers@gmail.com>
2021-05-28 07:35:14 -0500

    adding more images service tests

--- src/test/java/world/deslauriers/service/ImageServiceTest.java
+++ src/test/java/world/deslauriers/service/ImageServiceTest.java
@@ -1,9 +1,12 @@
 package world.deslauriers.service;
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest
 public class ImageServiceTest {
@@ -17,13 +20,22 @@ public class ImageServiceTest {
 
     // test database values
     private static final String ALBUM_2021 = "2021";
-    private static final String ALBUM_WRONG = "WRONG";
+    private static final String ALBUM_WRONG = "NOPE";
 
     @Test
     void testImageServiceCrud(){
-        //true
+
         var images = imageService.getImagesByAlbum(ALBUM_2021);
-        images.forEach(imageDto -> System.out.println(imageDto.toString()));
+        images.forEach(imageDto -> {
+            System.out.println(imageDto.toString());
+            assertNotNull(imageDto);
+            assertNotNull(imageDto.getId());
+            assertNotNull(imageDto.getFilename());
+            assertTrue(imageDto.getPublished());
+        });
+
+        images = imageService.getImagesByAlbum(ALBUM_WRONG);
+        assertNull(images);
     }
 
 }
