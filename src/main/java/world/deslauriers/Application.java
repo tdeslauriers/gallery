9314e76f43313493abff67d9a063f25d2d8da9be
tdeslauriers <thomas.deslauriers@gmail.com>
2022-05-19 07:50:35 -0500

    adding k8s annotation

--- src/main/java/world/deslauriers/Application.java
+++ src/main/java/world/deslauriers/Application.java
@@ -1,7 +1,20 @@
 package world.deslauriers;
 
+import io.dekorate.docker.annotation.DockerBuild;
+import io.dekorate.kubernetes.annotation.*;
 import io.micronaut.runtime.Micronaut;
 
+@KubernetesApplication(
+        name = "gallery",
+        serviceType = ServiceType.NodePort,
+        expose = true,
+        host = "localhost",
+        replicas = 3,
+        imagePullPolicy = ImagePullPolicy.Always,
+        labels = @Label(key = "app", value = "gallery"),
+        ports = @Port(name = "http", containerPort = 8081)
+)
+@DockerBuild(group = "tdeslauriers", name = "gallery")
 public class Application {
 
     public static void main(String[] args) {
