d7b6b9838d7ac3c39639350776db3b924693216a
tdeslauriers <thomas.deslauriers@gmail.com>
2022-05-25 06:50:27 -0500

    fixing k8s

--- src/main/java/world/deslauriers/Application.java
+++ src/main/java/world/deslauriers/Application.java
@@ -12,8 +12,18 @@ import io.micronaut.runtime.Micronaut;
         replicas = 3,
         imagePullPolicy = ImagePullPolicy.Always,
         labels = @Label(key = "app", value = "gallery"),
-        ports = @Port(name = "http", containerPort = 8081)
+        ports = @Port(name = "http", containerPort = 8080),
+        envVars = {
+            @Env(name = "GALLERY_JDBC_URL", configmap = "gallery-svc-config", value = "jdbc_url"),
+            @Env(name = "GALLERY_JDBC_USER", configmap = "gallery-svc-config", value = "jdbc_username"),
+            @Env(name = "GALLERY_JDBC_DRIVER", configmap = "gallery-svc-config", value = "jdbc_driver"),
+            @Env(name = "GALLERY_JDBC_DIALECT", configmap = "gallery-svc-config", value = "jdbc_dialect"),
+            @Env(name = "CORS_URL", configmap = "gallery-svc-config", value = "cors_url"),
+            @Env(name = "GALLERY_JDBC_PASSWORD", secret = "gallery-test-mariadb-galera", value = "mariadb-password"),
+            @Env(name = "JWT_GENERATOR_SIGNATURE_SECRET", secret = "jwt", value = "signature-pw"),
+        }
 )
+@DockerBuild(group = "tdeslauriers", name = "gallery")
 public class Application {
 
     public static void main(String[] args) {
